3fc6d89553802f480d074f15131c6bdea6cf396e
dcartwright2323 <31773100+dcartwright2323@users.noreply.github.com>
2021-06-30 14:22:19 -0400

    end of first php video

--- app/public/experiment.php
+++ app/public/experiment.php
@@ -1,2 +1,8 @@
-This is a test!
-This is a test again!
+<?php 
+    echo 2 + 2;
+    $myname = 'Brad';
+ ?>
+
+<h1>This page is all about <?php echo $myname ?></h1>
+<?php echo 5 * 5 ?>
+<h2>All about <?php echo $myname ?></h2>
